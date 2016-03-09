308b07abf115a93ee2be4ae716b8c1453604c58a
lzs0420 <79545660@qq.com>
2016-03-09 15:19:37 +0800

    1、my_common增加一些函数 2、增加地址选择器(使用select标签)

--- mine/my_common.js
+++ mine/my_common.js
@@ -132,3 +132,74 @@ function containsNOASC( s) {
     }
     return false;
 }
+
+/*倒计时*/
+function countDown(day,hour,minute,second){
+    if (second == 0) {
+        if(minute==0){
+            if(hour==0){
+                if(day==0){
+                    return
+                }else{
+                    day -- ;
+                    $(".day").html(day);
+                    hour = 23;
+                }
+            }else{
+                hour -- ;
+            }
+            $(".hour").html(hour);
+            minute = 59;
+        }else{
+            minute -- ;
+        }
+        $(".minute").html(minute);
+        second = 59;
+        $(".second").html(second);
+    } else {
+        second --;
+        $(".second").html(second);
+    }
+    setTimeout(function() {
+        countDown();
+    }, 1000);
+}
+
+/**只允许数字,e.g. $("#id").numeral();*/
+$.fn.numeral = function () {
+    $(this).css("ime-mode", "disabled");
+    this.bind("keypress", function (e) {
+        var code = (e.keyCode ? e.keyCode : e.which);  //兼容火狐 IE
+        if (!$.browser.msie && (e.keyCode == 0x8))  //火狐下 不能使用退格键
+        {
+            return;
+        }
+        return code >= 48 && code <= 57 || code == 46;
+    });
+    this.bind("blur", function () {
+        if (this.value.lastIndexOf(".") == (this.value.length - 1)) {
+            this.value = this.value.substr(0, this.value.length - 1);
+        } else if (isNaN(this.value)) {
+            this.value = " ";
+        }
+    });
+    this.bind("paste", function () {
+        var s = clipboardData.getData('text');
+        if (!/\D/.test(s));
+        value = s.replace(/^0*/, '');
+        return false;
+    });
+    this.bind("dragenter", function () {
+        return false;
+    }); //只允许输入数字，小数点
+    this.bind("keyup", function () {
+        this.value = this.value.replace(/[^\d.]/g, "");
+        //必须保证第一个为数字而不是.
+        this.value = this.value.replace(/^\./g, "");
+        //保证只有出现一个.而没有多个.
+        this.value = this.value.replace(/\.{2,}/g, ".");
+        //保证.只出现一次，而不能出现两次以上
+        this.value = this.value.replace(".", "$#$").replace(/\./g, "").replace("$#$", ".");
+    });
+};
+
