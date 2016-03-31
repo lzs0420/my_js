b3d820a558b7772df8cc123dc8aa36f617f2ee9d
lzs0420 <79545660@qq.com>
2016-03-31 16:37:58 +0800

    坑爹的HBuilder

--- util/lightbox/childbox.js
+++
@@ -1,8 +0,0 @@
-function lightbox_iframe_onload(){
-	$("#lightboxiframe",window.parent.document).height(document.body.scrollHeight);
-	$("#lightbox",window.parent.document).height(document.body.scrollHeight);
-	parent.lightboxposition();
-	parent.document.getElementById('lightboxiframe').style.visibility="visible";
-}
-lightbox_iframe_onload();
-
